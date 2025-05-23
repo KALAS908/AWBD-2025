86aec995b61eb71f22aba1320c29a3abb243f3d0
KALAS908 <tudor_haulica@yahoo.com>
2025-05-23 17:02:43 +0300

    Second commit

--- demo/demo/src/main/java/com/example/demo/Controller/MessagesController.java
+++ demo/demo/src/main/java/com/example/demo/Controller/MessagesController.java
@@ -2,15 +2,17 @@ package com.example.demo.Controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/messages")
 public class MessagesController {
 
-    @GetMapping("/messages")
+    @GetMapping
     public ResponseEntity<List<String>> messages()
     {
         return ResponseEntity.ok(Arrays.asList("first"));
